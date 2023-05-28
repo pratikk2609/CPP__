4093456bda9c893c8e31aaba72f2a2e63518bbe1
Pratik Pawar <pratikpawar2609@gmail.com>
2023-05-28 07:47:30 +0530

    new

---
+++ new3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "enter values of x and y: ";
+    double x;
+    double y;
+    cin >> x >> y;
+    cout << x + y;
+    return 0;
+}
