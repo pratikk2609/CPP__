a6c5a0c69b576209ce24ef953bf3dbae1beee770
Pratik Pawar <pratikpawar2609@gmail.com>
2023-06-15 21:04:58 +0530

    func

---
+++ func3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+int main()
+{
+
+    int sum;
+
+    sum = add(100, 78);
+
+    cout << "100 + 78 = " << sum << endl;
+
+    return 0;
+}
